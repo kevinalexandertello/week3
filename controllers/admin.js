ef56f496d7deaac5e20051e38a2d9a0033bba880
kevinalexandertello <gaston_alexander@hotmail.es>
2021-10-13 21:47:57 -0300

    yes

--- controllers/admin.js
+++ controllers/admin.js
@@ -23,7 +23,6 @@ exports.postAddProduct = (req, res, next) => {
   product
     .save()
     .then(result => {
-      // console.log(result);
       console.log('Created Product');
       res.redirect('/admin/products');
     })
