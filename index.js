618050eb54597d5f7232a44df72695a0c68554f5
kevinalexandertello <gaston_alexander@hotmail.es>
2021-10-14 01:27:32 -0300

    yes

--- index.js
+++ index.js
@@ -1,58 +1,104 @@
-const path = require('path');
+const path  = require('path');
+const flash =  require('connect-flash')
 
 const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-
-const errorController = require('./controllers/error');
-const User = require('./models/user');
-
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const csrf= require('csurf');
+const MongoDBStore = require('connect-mongodb-session')(session);
+
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+const authRoutes = require('./routes/auth')
+const errorController = require('./controllers/error')
+const User = require('./models/user')
+const cors = require('cors') // Place this with other requires (like 'path' and 'express')
+
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb+srv://kevin40597622:kevin40597622@cluster0.uzizy.mongodb.net/e-shop?retryWrites=true&w=majority';
+const MONGODB_URI = process.env.MONGODB_URL || 'mongodb+srv://kevin40597622:kevin40597622@cluster0.uzizy.mongodb.net/e-shop';
+
+const store = new MongoDBStore({
+  uri : MONGODB_URI,
+  collection: 'sessions'
+
+})
+
+const csrfProtection  = csrf()
+
+
+const corsOptions = {
+    origin: "https://week3-cse341.herokuapp.com/",
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    //useCreateIndex: true,
+    //useFindAndModify: false,
+    family: 4
+};
+
+                        
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
 
 app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set('views','views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,"public")));
+app.use(
+  session({
+  secret: 'my secret', 
+  resave : false ,
+  saveUninitialized: false,
+  store: store,
+}
+));
 
-app.use((req, res, next) => {
-    User.findById('5bab316ce0a7c75f783cb8a8')
-      .then(user => {
-        req.user = user;
-        next();
-      })
-      .catch(err => console.log(err));
-  });
+app.use(csrfProtection) 
+app.use(flash()) 
 
-app.use('/admin', adminRoutes);
+app.use((req,res,next)=>{
+  if(!req.session.user){
+    return next()
+  }
+  User.findById(req.session.user._id)
+  .then(user=>{
+    req.user= user;
+    next()
+  })
+  .catch(e=> console.log(e))
+})
+
+app.use( (req,res,next)=>{
+  res.locals.isLoggedIn = req.session.isLoggedIn;
+  res.locals.csrfToken =  req.csrfToken()
+  next()
+})
+
+
+
+app.use('/admin',adminRoutes.routes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
-app.listen(process.env.PORT || 3000);
 
 mongoose
-  .connect(
-    'mongodb+srv://kevin40597622:<kevin40597622>@cluster0.uzizy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-  )
-  .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Kevin',
-          email: 'gaston_alexander@hotmail.es',
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
-    });
-    app.listen(process.env.PORT || 3000);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+.connect(MONGODB_URL, options)
+.then(result=>{
+  app.listen(port)
+})
+.catch(e=> console.log(e))
