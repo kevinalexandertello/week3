9d2a7f629f8b4940818cd3eaec4ad7db4f459c48
kevinalexandertello <gaston_alexander@hotmail.es>
2021-10-27 22:30:06 -0300

    yes

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const store = new MongoDBStore({
   uri : MONGODB_URI,
   collection: 'sessions'
 
-})
+});
 
 const csrfProtection  = csrf()
 const app = express();
@@ -66,17 +66,23 @@ app.use(
 app.use(csrfProtection) 
 app.use(flash()) 
 
-app.use((req,res,next)=>{
-  if(!req.session.user){
-    return next()
+app.use((req, res, next) => {
+  // throw new Error('Sync Dummy');
+  if (!req.session.user) {
+    return next();
   }
   User.findById(req.session.user._id)
-  .then(user=>{
-    req.user= user;
-    next()
-  })
-  .catch(e=> console.log(e))
-})
+    .then(user => {
+      if (!user) {
+        return next();
+      }
+      req.user = user;
+      next();
+    })
+    .catch(err => {
+      next(new Error(err));
+    });
+});
 
 app.use( (req,res,next)=>{
   res.locals.isAuthenticated = req.session.isLoggedIn;
@@ -89,7 +95,18 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
+app.get('/500', errorController.get500);
 app.use(errorController.get404);
+
+app.use((error, req, res, next) => {
+  // res.status(error.httpStatusCode).render(...);
+  // res.redirect('/500');
+  res.status(500).render('500', {
+    pageTitle: 'Error!',
+    path: '/500',
+    isAuthenticated: req.session.isLoggedIn
+  });
+});
 
 mongoose
 .connect(MONGODB_URL, options)
